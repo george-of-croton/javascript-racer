e03691caff786a714aae6250d20f91734a38c2d1
George Armstrong <George@Georges-MacBook.telecom>
2016-03-13 22:24:45 +1300

    add new features

--- javaracer.js
+++ javaracer.js
@@ -7,9 +7,9 @@ var nopositions = document.get
 console.log(track.length);
 
 var player1  = 0; 
-var player1shoes = prompt("what type of shoes would Player 1 like? Adidas or Nike?");
+
 var player2 = 0;
-var player2shoes = prompt("what type of shoes would Player 2 like? Adidas or Nike")
+
 
 
 document.onkeyup =function(e)
@@ -20,23 +20,11 @@ document.onkeyup =function(e)
 		var positions = lane1.getElementsByTagName("td");
 		for( i = 0; i < positions.length; i++ ) {
 			if	(i === player1){
-
-				if (player1shoes == "nike" || "Nike"){
 				
 				positions[i].className = "";
 				positions[i + 1].className = "active";
 				player1 += 1;
 				break;
-			}
-
-			
-		
-			 else if ( player1shoes === "adidas" || "Adidas") {
-				positions[i].className = "";
-				positions[i + 2].className = "active";
-				player1 += 2;
-				break;
-				}
 			}
 			else if (player1 == (positions.length-1 || positions.length+1)){
 				alert("player 1 wins!");
